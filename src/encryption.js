d5ed2e88cfc51dc299ff1f5e89b3b28b10f1b307
José Ribeiro <jose@ae.studio>
2020-07-29 12:24:53 -0300

    refactor encryption module

--- src/encryption.js
+++ src/encryption.js
@@ -1,48 +1,83 @@
 const _sodium = require("libsodium-wrappers");
 
+let sodium;
+
+const getLibsodium = async () => {
+  if (!sodium) {
+    await _sodium.ready;
+    sodium = _sodium;
+  }
+  return sodium;
+};
+
+const getNonceByteLength = async () => {
+  const sodium = await getLibsodium();
+  return sodium.crypto_secretbox_NONCEBYTES;
+};
+
 async function randomEncryptionKey() {
-  await _sodium.ready;
-  const sodium = _sodium;
+  const sodium = await getLibsodium();
   return sodium.to_hex(
     sodium.randombytes_buf(sodium.crypto_secretbox_KEYBYTES)
   );
 }
-
-async function encryptContent(content, randomEncryptionKey) {
-  await _sodium.ready;
-  const sodium = _sodium;
-  let nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
-  let nonce_arr = sodium.to_hex(nonce);
-
-  return nonce_arr.concat(
-    sodium.to_hex(
-      sodium.crypto_secretbox_easy(
-        content,
-        nonce,
-        sodium.from_hex(randomEncryptionKey)
-      )
-    )
-  );
+async function randomNonce() {
+  const sodium = await getLibsodium();
+  const nonce = sodium.randombytes_buf(await getNonceByteLength());
+  const nonceHex = sodium.to_hex(nonce);
+  return [nonce, nonceHex];
 }
 
-async function decryptContent(nonce_and_ciphertext, key) {
-  await _sodium.ready;
-  const sodium = _sodium;
-  if (
-    nonce_and_ciphertext.length <
-    sodium.crypto_secretbox_NONCEBYTES + sodium.crypto_secretbox_MACBYTES
-  ) {
-    throw "Short message";
-  }
-  let nonce = sodium
-    .from_hex(nonce_and_ciphertext)
-    .slice(0, sodium.crypto_secretbox_NONCEBYTES);
-  let ciphertext = sodium
-    .from_hex(nonce_and_ciphertext)
-    .slice(sodium.crypto_secretbox_NONCEBYTES);
-  return sodium.to_string(
-    sodium.crypto_secretbox_open_easy(ciphertext, nonce, sodium.from_hex(key))
+async function encryptContent(unencryptedString, randomEncryptionKey) {
+  const sodium = await getLibsodium();
+
+  let [nonce, nonceHex] = await randomNonce();
+  const encryptionKeyArray = sodium.from_hex(randomEncryptionKey);
+
+  const encryptedString = sodium.crypto_secretbox_easy(
+    unencryptedString,
+    nonce,
+    encryptionKeyArray
   );
+
+  return nonceHex.concat(sodium.to_hex(encryptedString));
+}
+
+async function splitEncryptedString(encryptedString) {
+  const sodium = await getLibsodium();
+  const nonceAndCiphertext = sodium.from_hex(encryptedString);
+  const nonceByteLength = await getNonceByteLength();
+
+  return [
+    nonceAndCiphertext.slice(0, nonceByteLength),
+    nonceAndCiphertext.slice(nonceByteLength),
+  ];
+}
+
+const getMacByteLength = async () => {
+  const sodium = await getLibsodium();
+  return sodium.crypto_secretbox_MACBYTES;
+};
+
+async function decryptContent(nonceAndCiphertext, encryptionKey) {
+  const sodium = await getLibsodium();
+  const minimumEncryptedStringLengtht =
+    (await getNonceByteLength()) + (await getMacByteLength());
+
+  if (nonceAndCiphertext.length < minimumEncryptedStringLengtht) {
+    throw new Error("This message is too short to be decrypted.");
+  }
+
+  const [nonce, ciphertext] = await splitEncryptedString(nonceAndCiphertext);
+  const encryptionKeyArray = sodium.from_hex(encryptionKey);
+
+  const unencryptedContent = sodium.crypto_secretbox_open_easy(
+    ciphertext,
+    nonce,
+    encryptionKeyArray
+  );
+
+  return sodium.to_string(unencryptedContent);
 }
 
 module.exports = {
